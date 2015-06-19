a486a734c47dcee975f6bc1425162f150a2aa8ab
nigel <nigel@webbox.co.za>
2015-06-19 11:39:32 +0200

    implemented saving of receipts: curl -v -H "Accept: application/vnd.ereceipts.v1" -H "Content-type: application/json" -X POST   http://api.lvh.me:3000/receipts -d ' {"receipt":{"header":"header test", "footer":"footer test","receipt_number":"1", "receipt_items_attributes": [{"name":"sugar","price":"12.00"},{"name":"salt","price":"6.00"}]}}'

--- app/controllers/api/v1/receipts_controller.rb
+++ app/controllers/api/v1/receipts_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::ReceiptsController < ApplicationController
-  before_action :authenticate_with_token!, only: [:create, :update, :destroy]
+  before_action only: [:create, :update, :destroy]
   respond_to :json
 
   def index
@@ -7,11 +7,18 @@ class Api::V1::ReceiptsController < ApplicationController
   end
 
   def show
-    respond_with Receipt.find_by_user_id(params[:user_id])   
+    #respond_with Receipt.find_by_user_id(params[:user_id])   
   end
 
   def create
-    receipt = current_user.receipts.build(receipt_params) 
+    receipt = Receipt.new(receipt_params)
+    receipt_items = params[:receipt_items_attributes]
+    
+    # receipt_items.each do |item|
+    #   new_item = ReceiptItem.new()
+    #   new_item.save
+    # end
+
     if receipt.save
       render json: receipt, status: 201, location: [:api, receipt] 
     else
@@ -19,14 +26,14 @@ class Api::V1::ReceiptsController < ApplicationController
     end
   end
 
-  def update
-    receipt = current_user.receipts.find_by_id(params[:id])
-    if receipt.update(receipt_params)
-      render json: receipt, status: 200, location: [:api, receipt] 
-    else
-      render json: { errors: receipt.errors }, status: 422
-    end
-  end
+  # def update
+  #   receipt = receipts.find_by_id(params[:id])
+  #   if receipt.update(receipt_params)
+  #     render json: receipt, status: 200, location: [:api, receipt] 
+  #   else
+  #     render json: { errors: receipt.errors }, status: 422
+  #   end
+  # end
 
   def destroy
     receipt = current_user.receipts.find_by_id(params[:id]) 
@@ -37,7 +44,7 @@ class Api::V1::ReceiptsController < ApplicationController
   private
 
     def receipt_params
-      params.require(:receipt).permit(:user_id, :receipt_number, :header, :footer) 
+      params.require(:receipt).permit(:user_id,:receipt_number, :header, :footer, :receipt_items_attributes) 
     end
 
 end
--- app/models/receipt.rb
+++ app/models/receipt.rb
@@ -1,6 +1,9 @@
 class Receipt < ActiveRecord::Base
-  validates :header, :footer, :user_id, presence: true
+  has_many :receipt_items
+  validates :header, :footer, presence: true
   validates :receipt_number, numericality: { greater_than_or_equal_to: 0 },
                     presence: true
-  belongs_to :user
+  accepts_nested_attributes_for :receipt_items, :allow_destroy => true
+  belongs_to :organisation
+
 end
